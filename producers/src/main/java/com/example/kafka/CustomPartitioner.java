35d0f830c7bf0aaf391feadc20c9d089432585e6
aladindev <kjune0205@naver.com>
2024-08-24 13:52:57 +0900

    Customer Partitioner

--- producers/src/main/java/com/example/kafka/CustomPartitioner.java
+++ producers/src/main/java/com/example/kafka/CustomPartitioner.java
@@ -1,4 +1,25 @@
 package com.example.kafka;
 
-public class CustomPartitioner {
+import org.apache.kafka.clients.producer.Partitioner;
+import org.apache.kafka.clients.producer.internals.DefaultPartitioner;
+import org.apache.kafka.common.Cluster;
+import org.apache.kafka.common.replica.PartitionView;
+
+import java.util.Map;
+
+public class CustomPartitioner implements Partitioner {
+    @Override
+    public int partition(String topic, Object key, byte[] keyBytes, Object value, byte[] valueBytes, Cluster cluster) {
+        return 0;
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public void configure(Map<String, ?> configs) {
+
+    }
 }
