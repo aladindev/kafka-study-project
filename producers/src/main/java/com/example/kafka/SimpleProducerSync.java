cc1fd8d2520d04ae742acf782b5dcffc8ea63beb
aladindev <kjune0205@naver.com>
2024-08-24 13:45:30 +0900

    Customer Partitioner

--- producers/src/main/java/com/example/kafka/SimpleProducerSync.java
+++ producers/src/main/java/com/example/kafka/SimpleProducerSync.java
@@ -22,7 +22,7 @@ public class SimpleProducerSync {
         // key:null, value:"hello world"
         Properties props = new Properties();
         //bootstrap.servers, key.serializer.class, value.serializer.class
-        props.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, serverIp;
+        props.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, serverIp);
         props.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         props.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
