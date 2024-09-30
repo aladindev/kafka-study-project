338956bc1d1c87fc1bc2d6c3d7df2756982d1907
aladindev <kjune0205@naver.com>
2024-09-30 14:44:41 +0900

    Kafka CDC Source Producer

--- producers/src/main/java/com/example/kafka/SourceConnectorAPI.java
+++ producers/src/main/java/com/example/kafka/SourceConnectorAPI.java
@@ -126,33 +126,6 @@ public class SourceConnectorAPI { // Oracle -> Oracle Source Connector -> Kafka 
         return out.toByteArray();
     }
 }
-//                "        \"field1\": \"TEST Field1\",\n" +
-//                "        \"field2\": \"TEST Field2\"\n" +
-//                "    }\n" +
-//                "}";
-//
-//        JSONObject jsonObject = new JSONObject(value);
-//        // ProducerRecord 생성
-//        // JSON 객체 생성
-////        TestJsonClass jsonData = new TestJsonClass();
-////        jsonData.setId(1);
-////        jsonData.setField1("test");
-////        jsonData.setField2("testvalue");
-//
-//        // ProducerRecord 생성 및 전송
-//        ProducerRecord<String, String> record = new ProducerRecord<>(topic, "1", jsonObject.toString());
-////        ProducerRecord<String, TestJsonClass> record = new ProducerRecord<>(topic, key, jsonData);
-//        //ProducerRecord<String, String> record = new ProducerRecord<>(topic, key, value);
-//
-//        // 데이터 전송
-//        producer.send(record, (RecordMetadata metadata, Exception e) -> {
-//            if (e != null) {
-//                e.printStackTrace();
-//                System.out.println("exeception : " + e.getMessage());
-//            } else {
-//                System.out.println("Sent record: " + metadata);
-//            }
-//        });
 //
 //        // Producer 종료
 //        producer.close();
