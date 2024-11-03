2570f97851658f7d40a0cb36c73621b428effbf5
aladindev <kjune0205@naver.com>
2024-11-03 17:22:40 +0900

    Kafka CDC Source Producer

--- producers/src/main/java/com/example/kafka/SourceConnectorAPI.java
+++ producers/src/main/java/com/example/kafka/SourceConnectorAPI.java
@@ -104,8 +104,8 @@ public class SourceConnectorAPI { // Oracle -> Oracle Source Connector -> Kafka 
                 ));
         jsonPayload.put("payload", new JSONObject()
                 .put("id", "00001")
-                .put("field1", "restart5 Field1")
-                .put("field2", "restart5 Field2")
+                .put("field1", "restart6 Field1")
+                .put("field2", "restart6 Field2")
         );
 
         System.out.println(jsonPayload.toString());
