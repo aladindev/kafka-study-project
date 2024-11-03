7aabd0576a0c9bad69e22281884b27ea217da1e4
aladindev <kjune0205@naver.com>
2024-11-03 15:23:59 +0900

    Kafka CDC Source Producer

--- producers/src/main/java/com/example/kafka/SourceConnectorAPI.java
+++ producers/src/main/java/com/example/kafka/SourceConnectorAPI.java
@@ -81,7 +81,7 @@ public class SourceConnectorAPI { // Oracle -> Oracle Source Connector -> Kafka 
 
         // Kafka Producer 설정
         Properties props = new Properties();
-        props.put("bootstrap.servers", serverIp);
+        props.put("bootstrap.servers", serverIp);   
         props.put("key.serializer", StringSerializer.class);
         props.put("value.serializer", StringSerializer.class); // JSON 문자열로 전송
 
@@ -123,6 +123,6 @@ public class SourceConnectorAPI { // Oracle -> Oracle Source Connector -> Kafka 
         Encoder encoder = EncoderFactory.get().binaryEncoder(out, null);
         datumWriter.write(record, encoder);
         encoder.flush();
-        return out.toByteArray();  
+        return out.toByteArray();
     }
 }              
