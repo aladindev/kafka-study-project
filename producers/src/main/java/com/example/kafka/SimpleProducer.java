25b6ca7d5c1f49785be68d1c8f31daf96fd86c37
aladindev <kjune0205@naver.com>
2025-01-12 16:16:18 +0900

    Kafka CDC Source Producer

--- producers/src/main/java/com/example/kafka/SimpleProducer.java
+++ producers/src/main/java/com/example/kafka/SimpleProducer.java
@@ -20,8 +20,8 @@ public class SimpleProducer {
         // key:null, value:"hello world"
         Properties props = new Properties();
         //bootstrap.servers, key.serializer.class, value.serializer.class
-        props.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, serverIp);                            
-        props.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, serverIp);
+        props.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());  
         props.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
         //KafkaProducer Object create
