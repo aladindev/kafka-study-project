4aae60a72a5e0141636acea6085bfe9b084a70ea
aladindev <kjune0205@naver.com>
2025-01-28 18:41:40 +0900

    Kafka CDC Source Producer

--- producers/src/main/java/com/example/kafka/SimpleProducer.java
+++ producers/src/main/java/com/example/kafka/SimpleProducer.java
@@ -27,7 +27,7 @@ public class SimpleProducer {
         //KafkaProducer Object create
         KafkaProducer<String, String> kafkaProducer = new KafkaProducer<String, String>(props);
 
-        //ProducerRecord Object create      
+        //ProducerRecord Object create        
         ProducerRecord<String, String> producerRecord = new ProducerRecord<>(topicName, "hello world2");
 
         //KafkaProducer message send
